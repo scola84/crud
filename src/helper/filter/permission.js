579cc1d36d112a9a1c6018be5ed5beb6770903ad
M.C. van den Boogaart <scola@kvdb.name>
2018-05-30 23:30:37 +0200

    Allow dynamic permission

--- src/helper/filter/permission.js
+++ src/helper/filter/permission.js
@@ -1,7 +1,9 @@
 export default function filterPermission(permission) {
   return (name) => {
-    return (request) => {
-      return request.user.may(permission[name]);
+    return (route, data) => {
+      return typeof permission[name] === 'function' ?
+        permission[name](route, data) :
+        route.user.may(permission[name]);
     };
   };
 }
