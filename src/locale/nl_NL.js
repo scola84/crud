c6447d1fa5923541da5a3543c4117cc025d3ab41
M.C. van den Boogaart <scola@kvdb.name>
2018-06-01 17:32:01 +0200

    Add summary title clicks, add strings

--- src/locale/nl_NL.js
+++ src/locale/nl_NL.js
@@ -10,12 +10,14 @@ export default {
       button: {
         add: 'Toevoegen',
         back: 'Terug',
+        call: 'Bellen',
         cancel: 'Annuleren',
         close: 'Sluiten',
         clr: 'Verwijderen',
         del: 'Verwijderen',
         done: 'Klaar',
         edit: 'Wijzigen',
+        mail: 'Mailen',
         open: 'Openen',
         save: 'Opslaan',
         send: 'Verzenden'
--- src/view/summary.js
+++ src/view/summary.js
@@ -1,7 +1,21 @@
 import { GraphicWorker } from '@scola/gui';
+import { event, select } from 'd3';
+import { handleGui } from '../helper';
 
 export default class SummaryClicker extends GraphicWorker {
   act(route, data, callback) {
+    route.summary
+      .selectAll('.primary:empty + .secondary .title :not(.l3) a')
+      .on('click', (d, i, n) => {
+        event.preventDefault();
+
+        const handler = handleGui({
+          summary: select(n[i]).attr('href').slice(2)
+        })();
+
+        handler(d, i, n, { name: 'summary' });
+      });
+
     route.summary
       .selectAll('.actions li')
       .on('click', (d, i, n) => {
