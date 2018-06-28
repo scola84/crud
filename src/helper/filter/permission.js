a8239b2c681421ae96ee46e3da7de4f7cdfdf06d
M.C. van den Boogaart <scola@kvdb.name>
2018-06-28 16:50:57 +0200

    Move permission check to user

--- src/helper/filter/permission.js
+++ src/helper/filter/permission.js
@@ -1,13 +1,7 @@
-import filterDisabler from './disabler';
-
 export default function filterPermission(permissions) {
-  const filter = filterDisabler();
-
   return (name) => {
-    const permission = permissions[name];
-
     return (route, data) => {
-      return filter(route, data, permission);
+      return route.user.may(permissions[name], route, data);
     };
   };
 }
