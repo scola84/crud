d6af2d6e558f5422a3b5cd0408255e4401f9c5d0
M.C. van den Boogaart <scola@kvdb.name>
2018-07-06 21:19:57 +0200

    Fix pass call in select view merger

--- src/select/merger.js
+++ src/select/merger.js
@@ -3,6 +3,6 @@ import { GraphicWorker } from '@scola/gui';
 export default class ViewMerger extends GraphicWorker {
   act(route, data, callback) {
     route.checked = Array.isArray(data.data) ? data.data : [data.data];
-    this.pass(route, callback);
+    this.pass(route, data, callback);
   }
 }
